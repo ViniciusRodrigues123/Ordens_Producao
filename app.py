233d75b87d24685649f1eb2bbf3140e1803c6c60
ViniciusRodrigues123 <vinicius_rodrigue153@estudante.sesisenai.org.br>
2026-03-31 09:19:59 -0300

    Commit 2

--- app.py
+++ app.py
@@ -57,4 +57,24 @@ if __name__=='__main__':
     
     app.run(debug=True, host='0.0.0.0', port=5000)
         
- 
+# Rota oir ID - Buscar uma ordem específica pelo ID(GET)
+
+@app.route('/ordens/<int:ordem_id>, methods=['GET']')
+
+def buscar_ordem(ordem_id):
+    '''
+    Buscar uma única ordem de produção pelo ID.
+    Parametros de URL:
+        - ordem id(int): ID da ordem a ser buscada.
+        
+    Retornar:
+        200 + JSON da ordem, se for encontrada
+        404 + mensagem de erro, se não existir
+    '''
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute('SELECT * FROM ordens WHERE ID = ')
+    ordem = cursor.fetchone() # ele retorna um único registro ou None
+    conn.close()
+
