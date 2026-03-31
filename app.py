5b1d335ca4a5909cb69d348b84eaf764f0851e18
ViniciusRodrigues123 <vinicius_rodrigue153@estudante.sesisenai.org.br>
2026-03-31 14:07:29 -0300

    Commit 3

--- app.py
+++ app.py
@@ -12,13 +12,19 @@ app = Flask(__name__, static_folder='static', static_url_path='')
 # Habilitar os CORS
 CORS(app)
 
+#---------------------------
 # Rota N1 - Página Inicial
+#---------------------------
+
 @app.route('/')
 def index():
     # ALIMENTAR O ARQUIVO INDEX.HTML DA PASTA STATIC
     return app.send_static_file('index.html')
 
+#---------------------------
 # Rota N2 - Status API
+#---------------------------
+
 @app.route('/status')
 def status():
     '''
@@ -31,8 +37,11 @@ def status():
         "versao": "1.0.0",
         "mensagem": "Ola, Fabrica, API FUNCIONANDO!"
     })
-
+    
+#---------------------------
 # Rota N3 - Listar todas as Ordens(GET)
+#---------------------------
+
 @app.route('/ordens', methods=['GET'])
 def listar_ordens():
     '''
@@ -50,16 +59,11 @@ def listar_ordens():
     # Converte cada Row do SQLite em dicionario Python para serializar em JSON
     return jsonify([dict(o) for o in ordens])
 
-# PONTO DE PARTIDA
+#---------------------------
+# Rota por ID - Buscar uma ordem específica pelo ID(GET)
+#---------------------------
 
-if __name__=='__main__':
-    init_bd()
-    
-    app.run(debug=True, host='0.0.0.0', port=5000)
-        
-# Rota oir ID - Buscar uma ordem específica pelo ID(GET)
-
-@app.route('/ordens/<int:ordem_id>, methods=['GET']')
+@app.route('/ordens/<int:ordem_id>', methods=['GET'])
 
 def buscar_ordem(ordem_id):
     '''
@@ -74,7 +78,67 @@ def buscar_ordem(ordem_id):
     conn = get_connection()
     cursor = conn.cursor()
     
-    cursor.execute('SELECT * FROM ordens WHERE ID = ')
+    cursor.execute('SELECT * FROM ordens WHERE ID = ?', (ordem_id, ))
     ordem = cursor.fetchone() # ele retorna um único registro ou None
     conn.close()
 
+    if ordem is None:
+        return jsonify({'erro': f'Ordem {ordem_id} nao encontrada.'}), 404
+    return jsonify(dict(ordem)), 200
+
+#---------------------------
+# Rota: Criar nova ordem de rota
+#---------------------------
+
+@app.route('ordens', methods=['POST'])
+def criar_ordem():
+    '''
+    Cria uma nova ordem de produção a partir dos dados JSON enviados.
+    
+    Body esperado(JSON):
+    
+        produto     (str) : Nome do produto      - Obrigatório
+        quantidade  (int) : Quantidade de peças  - Obrigatório, > 0
+        status      (str) : Opcional             - Padrão: 'Pendente'
+        
+    Retorno:
+        201 : JSON da ordem criada, em caso de sucesso.
+        400 : mensagem de erro, se dados inválidos
+    '''
+    
+    dados = request.get_json()
+
+    if not dados:
+        return jsonify({'erro': 'Body da requisicao ausente ou invalido'}),
+
+    #verificação de campo obrigatório (produto)
+    produto = dados.get('produto', '').strip()
+    if not produto:
+        return jsonify({'erro': 'Campo "Produto" e obrigatorio e nao pode ser vazio'}), 400
+    
+    #verificação de campo obrigatório (quantidade)
+    quantidade = dados.get('quantidade')
+    if quantidade is None:
+        return jsonify({'erro': 'Campo "quantidade" e obrigatorio.'}), 400
+    
+    # Verifica se a quantidade é um número inteiro e positivo
+    try:
+        quantidade = int(quantidade)
+        if quantidade <= 0:
+            raise ValueError
+    except (ValueError, TypeError):
+        return jsonify({'erro': 'Campo "quantidade" deve ser um numero inteiro e positivo.'}), 400
+    
+    # Status (*pendente, em andamento, concluída) - opicional
+    status_validos = ['Pendente','Em andamento','Concluida']
+    status = dados.get('status', 'Pendente')
+    if status not in status_validos:
+        return jsonify({'erro': f'Status invalido. Use {status_validos}'}), 400
+    
+# ------- PONTO DE PARTIDA -------
+
+if __name__=='__main__':
+    init_bd()
+    
+    app.run(debug=True, host='0.0.0.0', port=5000)
+        
